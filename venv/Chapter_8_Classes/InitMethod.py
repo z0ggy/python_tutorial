c9a1e600cdb6c59b84dc14c98bdb6e68a398de42
z0ggy <zoggy76@gmail.com>
2021-07-15 21:51:32 +0100

    Classes

---
+++ venv/Chapter_8_Classes/InitMethod.py
@@ -0,0 +1,7 @@
+"""
+init method allows every instance of the class to be created or initialized with
+specific parameters predetermined at the creation of that object. What it does it set the attribute for the object
+for example the characteristics if I had a class for person I might want to set the persons first name and last name and
+perhaps a telephone number or an address the init
+"""
+# Self variable represents an instance of the class
---
+++ venv/Chapter_8_Classes/Person.py
@@ -0,0 +1,52 @@
+import random
+
+
+class Person:
+    def __init__(self, firstname, lastname, health, status):
+        """initialize attributes to be used in/available for all of my class
+        class methods in this class, and for class objects created by this class."""
+
+        self.firstname = firstname
+        self.lastname = lastname
+        self.health = health
+        self.status = status
+
+    def introduce(self):
+        """All people introduce themselves"""
+        print("Hello, my name is {} {}".format(self.firstname, self.lastname))
+
+    def emote(self):
+        emotion = random.randrange(1, 3)
+        if emotion == 1:
+            print("{} is happy today".format(self.firstname))
+        elif emotion == 2:
+            print("{} is sad right now".format(self.firstname))
+
+    def status_change(self):
+        if self.health == 100:
+            print("{} is totally healthy!".format(self.firstname))
+        elif self.health >= 76:
+            print("{} is a little tired today".format(self.firstname))
+        elif self.health >= 51:
+            print("{} feels unwell.".format(self.firstname))
+        elif self.health <= 40:
+            print("{} goes to the doctor".format(self.firstname))
+        else:
+            print("{} is unconscious".format(self.firstname))
+
+
+Maria = Person("Maria", "Masyna", 99, status=True)
+Rey = Person("Rey", "Jones", 88, status=False)
+Lee = Person("Lee", "Williams", 72, status=True)
+
+print("{} is my friend? {}".format(Maria.firstname, Maria.status))
+print("{} is my friend? {}".format(Rey.firstname, Rey.status))
+
+
+Maria.introduce()
+Rey.introduce()
+Lee.introduce()
+
+Maria.status_change()
+Rey.status_change()
+Lee.status_change()
